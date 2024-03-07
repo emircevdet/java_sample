22aff89aa0392fe4566de8617470a9dce892201d
Emir Cevdet Ünsal <145847994+emircevdet@users.noreply.github.com>
2024-03-07 19:45:14 +0300

    Update ortalamaHesaplama.java

--- src/ortalamaHesaplama.java
+++ src/ortalamaHesaplama.java
@@ -5,33 +5,33 @@ public class ortalamaHesaplama {
         String[] isimler;
         Scanner giris = new Scanner(System.in);
 
-        // Kişi sayısını alma
+        
         System.out.println("Kişi Sayısını Giriniz:");
         kisiSayisi = giris.nextInt();
 
-        // İsimlerin alınması
+        
         isimler = new String[kisiSayisi];
         for (int i = 0; i < kisiSayisi; i++) {
             System.out.println((i + 1) + ". Kişinin Adını Giriniz:");
             isimler[i] = giris.next();
         }
 
-        // Her bir kişi için notların alınması ve hesaplanması
+        
         for (int i = 0; i < kisiSayisi; i++) {
             int vn, fn, bn;
             String harfNotu = "";
             String gecme = "";
 
-            // Vize ve final notlarını alma
+            
             System.out.println(isimler[i] + " için Vize Notunu Giriniz:");
             vn = giris.nextInt();
             System.out.println(isimler[i] + " için Final Notunu Giriniz:");
             fn = giris.nextInt();
 
-            // bn değerini hesaplama
+           
             bn = (int) ((vn * 0.4 + fn * 0.6));
 
-            // Harf notu ve geçme durumunu hesaplama
+           
             if (bn <= 28) {
                 harfNotu = "FF";
                 gecme = "Kaldı";
@@ -66,7 +66,7 @@ public class ortalamaHesaplama {
 
             }
 
-            // Sonuçları yazdırma
+           
             System.out.println(isimler[i] + " için Harf Notu: " + harfNotu + "\n" + isimler[i] + " için Geçme Durumu: " + gecme);
         }
     }
